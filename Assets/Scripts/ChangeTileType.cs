36ef964f394be29fdf3666508a00a8bfe538d74a
MartinForsmark <70843057+MartinForsmark@users.noreply.github.com>
2020-12-19 17:44:04 +0100

    added 2 scripts for changing tile types when clicked

---
+++ Assets/Scripts/ChangeTileType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChangeTileType : MonoBehaviour
+{
+    public StoreColorData ColorData;
+
+    public void Start()
+    {
+        ColorData = FindObjectOfType<StoreColorData>();
+    }
+
+    public void ChangeTileColor()
+     {
+         EventSystem.current.currentSelectedGameObject.GetComponent<Image>().color = ColorData.ChangeColor; // checks what game object that is pressed/clicked on
+     }
+}
---
+++ Assets/Scripts/StoreColorData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreColorData : MonoBehaviour
+{
+    public Color ChangeColor;
+    
+    
+    public void ChangeWaterColor()
+    {
+        ChangeColor = Color.blue;            
+    }
+
+    public void ChangeGrassColor()
+    {
+        ChangeColor = Color.green;
+    } 
+}
