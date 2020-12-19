765d2f08690bb456336d7815582ecbf3a3d96d83
MartinForsmark <70843057+MartinForsmark@users.noreply.github.com>
2020-12-19 19:58:27 +0100

    added InputField Name Bars for the canvas

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour
+{
+
+    public GameObject inputFieldColor;
+    public GameObject inputFieldColor2;
+    public GameObject Water;
+   
+
+
+    public void changeColor()
+    {
+        if (inputFieldColor.GetComponent<InputField>().text == "Fire")
+        {
+            Water.GetComponent<Image>().color = Color.red;
+        }
+    }
+}
--- Assets/Scripts/TileName.cs
+++ Assets/Scripts/TileName.cs
@@ -8,32 +8,20 @@ public class TileName : MonoBehaviour
 {
     public string Name;
     public string Name2;
-    public GameObject inputField;
-    public GameObject inputField2;
+    public GameObject inputFieldName;
+    public GameObject inputFieldName2;
     public Text WaterText;
     public Text GrassText;
-    public GameObject Water;
-    public GameObject Grass;
 
     public void StoreName()
     {
-        Name = inputField.GetComponent<InputField>().text;
+        Name = inputFieldName.GetComponent<InputField>().text;
         WaterText.text = Name;
-        if (inputField.GetComponent<InputField>().text == "Fire")
-        {
-            Water.GetComponent<Image>().color = Color.red;
-        }
-
     }
 
     public void StoreName2()
     {
-        Name2 = inputField2.GetComponent<InputField>().text;
+        Name2 = inputFieldName2.GetComponent<InputField>().text;
         GrassText.text = Name2;
-        if (inputField2.GetComponent<InputField>().text == "Fire")
-        {
-            Grass.GetComponent<Image>().color = Color.red;
-        }
     }
-
 }
