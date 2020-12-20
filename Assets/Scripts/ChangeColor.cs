eb5c5dbaa957483e4af3f0038579b35423fc0ad9
MartinForsmark <70843057+MartinForsmark@users.noreply.github.com>
2020-12-20 13:36:54 +0100

    Added Color Options for the preview

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -7,16 +7,45 @@ public class ChangeColor : MonoBehaviour
 {
 
     public GameObject inputFieldColor;
-    public GameObject inputFieldColor2;
-    public GameObject Water;
-   
-
+    public GameObject imageColor;
 
     public void changeColor()
     {
-        if (inputFieldColor.GetComponent<InputField>().text == "Fire")
-        {
-            Water.GetComponent<Image>().color = Color.red;
+        if (inputFieldColor.GetComponent<InputField>().text == "Red") {
+            imageColor.GetComponent<Image>().color = Color.red;
         }
+        
+        else if(inputFieldColor.GetComponent<InputField>().text == "Pink") {
+            imageColor.GetComponent<Image>().color = Color.magenta;
+        }
+        
+        else if(inputFieldColor.GetComponent<InputField>().text == "Yellow") {
+            imageColor.GetComponent<Image>().color = Color.yellow;
+        }
+        
+        else if(inputFieldColor.GetComponent<InputField>().text == "Blue") {
+            imageColor.GetComponent<Image>().color = Color.blue;
+        }
+        
+        else if(inputFieldColor.GetComponent<InputField>().text == "Black") {
+            imageColor.GetComponent<Image>().color = Color.black;
+        }
+        
+        else if (inputFieldColor.GetComponent<InputField>().text == "Gray") {
+            imageColor.GetComponent<Image>().color = Color.gray;
+        }
+        
+        else if(inputFieldColor.GetComponent<InputField>().text == "Cyan") {
+                imageColor.GetComponent<Image>().color = Color.cyan;
+        }
+        
+        else if(inputFieldColor.GetComponent<InputField>().text == "White") {
+            imageColor.GetComponent<Image>().color = Color.white;
+        }
+        
+        else if(inputFieldColor.GetComponent<InputField>().text == "Green") {
+            imageColor.GetComponent<Image>().color = Color.green;
+        }
+        
     }
 }
